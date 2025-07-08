c91882699f325a6eba395d2a1bdd4be6f04ee0f5
MakarchukDev <makarchuk.polysan@gmail.com>
2025-07-08 16:32:31 +0300

    Update site content

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const textElements = document.querySelectorAll(".animate-text");
-    textElements.forEach((element) => {
-        element.style.opacity = 0;
-        element.style.transition = "opacity 2s ease-in-out";
-        setTimeout(() => (element.style.opacity = 1), 500);
-    });
-});
